4fbc3bdbd2a95e287003950c6403a8e0542db446
Mohd Zaheer Uddin <sohailkhannn.0525@gmail.com>
2026-08-24 14:21:26 +0530

    agentx 2.0.7

--- Formula/agentx.rb
+++ Formula/agentx.rb
@@ -1,14 +1,17 @@
 class Agentx < Formula
   desc "AI agent that builds production-ready websites from your terminal"
   homepage "https://github.com/SohailKhan0525/agentx-cli"
-  url "https://registry.npmjs.org/@agent-qofeno/agentx-cli/-/agentx-cli-2.0.6.tgz"
-  sha256 "0b6802d65f2ad23368d9e436c101a3acf849f55180ec8ccde786d26246ee2b40"
+  url "https://registry.npmjs.org/@agent-qofeno/agentx-cli/-/agentx-cli-2.0.7.tgz"
+  sha256 "72d4634784472bb3e820bbbab115b548b92c38f3cba33b90656cb6c37f3b195a"
   license "MIT"
+
   depends_on "node"
+
   def install
     system "npm", "install", *Language::Node.std_npm_install_args(libexec)
     bin.install_symlink Dir["#{libexec}/bin/*"]
   end
+
   test do
     system "#{bin}/agentx", "--version"
   end
